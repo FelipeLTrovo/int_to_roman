a00485e9d7f3efa0ea9e00a64f6ffe2fc6c9f36f
FelipeLTrovo <felipeltrovo@hotmail.com>
2022-12-18 17:06:48 -0300

    Move logic to a command.

--- commands/integer_to_roman.rb
+++ commands/integer_to_roman.rb
@@ -1,15 +1,34 @@
-require './constants/roman_numerals'
+require_relative '../constants/roman_numerals'
 
-@outcome = ''
+class IntegerToRoman
+  def initialize(number)
+    @number = number
+    @outcome = ''
+  end
 
-def int_to_roman(number)
-  ROMAN_NUMERALS.each do |key, value|
-    until number < value do
-      number = number - value
-      @outcome << key
+  def call
+    return check_int unless @number.is_a?(Integer)
+    return check_zero if @number.zero?
+    int_to_roman(@number)
+    @outcome
+  end
+
+  private
+
+  def check_int
+    "Only Integer parameters are allowed."
+  end
+
+  def check_zero
+    "Roman numerals don't have the number zero."
+  end
+
+  def int_to_roman(number)
+    ROMAN_NUMERALS.each do |key, value|
+      until number < value do
+        number = number - value
+        @outcome << key
+      end
     end
   end
 end
-
-int_to_roman(500)
-puts @outcome
