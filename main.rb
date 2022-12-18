8d8992d74c3282a3d572b690d4a1609fb415adf1
FelipeLTrovo <felipeltrovo@hotmail.com>
2022-12-18 17:34:41 -0300

    Creates driver class to run the command in a shell prompt.

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative './commands/integer_to_roman'
+require 'colorize'
+
+
+while true do
+  puts "\nPlease type an Integer: ".blue
+  param = gets.chomp
+  param = param.to_i if(param == '0' || param.to_i > 0)
+  cmd = IntegerToRoman.new(param).call
+  puts "\nResult: #{cmd}".green
+  puts "\nType Ctrl + C to exit the program.".red
+end
