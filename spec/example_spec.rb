173f365a35ffd83146f5f61884b4a5fa35376a2b
FelipeLTrovo <felipeltrovo@hotmail.com>
2022-12-18 17:05:58 -0300

    Adds tests for IntegerToRoman command.

--- spec/example_spec.rb
+++
@@ -1,5 +0,0 @@
-describe 'ExampleSpec' do
-  it 'is true' do
-    expect(true).to be true
-  end
-end
---
+++ spec/integer_to_roman_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../commands/integer_to_roman'
+
+describe 'IntegerToRoman' do
+  describe "With valid params" do
+    it 'is true' do
+      cmd_a = IntegerToRoman.new(3).call
+      cmd_b = IntegerToRoman.new(39).call
+      cmd_c = IntegerToRoman.new(246).call
+      cmd_d = IntegerToRoman.new(789).call
+      cmd_e = IntegerToRoman.new(2421).call
+
+      expect(cmd_a).to eq('III')
+      expect(cmd_b).to eq('XXXIX')
+      expect(cmd_c).to eq('CCXLVI')
+      expect(cmd_d).to eq('DCCLXXXIX')
+      expect(cmd_e).to eq('MMCDXXI')
+    end
+  end
+
+  describe "With invalid params" do
+    it 'is zero' do
+      cmd = IntegerToRoman.new(0).call
+
+      expect(cmd).to eq("Roman numerals don't have the number zero.")
+    end
+
+    it 'is a string' do
+      cmd_a = IntegerToRoman.new("123").call
+      cmd_b = IntegerToRoman.new("some text").call
+      cmd_c = IntegerToRoman.new(2.5).call
+
+      expect(cmd_a).to eq("Only Integer parameters are allowed.")
+      expect(cmd_b).to eq("Only Integer parameters are allowed.")
+      expect(cmd_c).to eq("Only Integer parameters are allowed.")
+    end
+  end
+end
